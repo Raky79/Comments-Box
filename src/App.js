73b5d0485ffffce727298a476e17d70926df20f0
Raquel Revelles <sadierev@hotmail.com>
2023-01-04 09:51:52 +0000

    edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
      <h1>
-      Comment Box in the making!
+      Leave your comments below:
      </h1>
      <Comments currentUserId="1"/>
     </div>
--- src/comments/Comments.js
+++ src/comments/Comments.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
-import {getComments as getCommentsApi, createComment as createCommentApi, deleteComment as deleteCommentApi} from '../api';
+import {getComments as getCommentsApi, createComment as createCommentApi, deleteComment as deleteCommentApi, updateComment as updateCommentApi} from '../api';
 import Comment from './Comment'; 
 import CommentForm from './CommentForm'; 
 
 const Comments  = ({currentUserId}) => {     // we need to pass in our props the currentUserId (in App.js -- <Comments currentUserId="1"/>) which will pass from our parent
     
     const [backendComments, setbackendComments] = useState([]);  // the state where we will store all the comments we get from the backend . An empty array by default as we don't have any data and the backend comments are blank 
-   
+    const [activeComment, setActiveComment] = useState(null)
     const rootComments = backendComments.filter(
         backendComment => backendComment.parentId === null
         );              // we take this comments with parentId === null which means this comments don't have parentId which is why we are filtering by this predicate(a function that returns a single TRUE or FALSE) [backendComment.parentId === null)], and we are accessing this backendComment 
@@ -18,9 +18,10 @@ const Comments  = ({currentUserId}) => {     // we need to pass in our props the
     };
 
     const addComment = (text, parentId) => {
-        console.log("addComment", text, parentId);     // parentId , because when we create a reply we create a comment which is the child of another comment
+        // console.log("addComment", text, parentId);     // parentId , because when we create a reply we create a comment which is the child of another comment
         createCommentApi(text, parentId).then(comment => {
             setbackendComments([comment, ...backendComments]); // we want our new comment to be at the beginning , this is why first the comment and after that spreading the backendComments that we already have 
+            setActiveComment(null);    // this makes the bos form disapear when we submit a reply 
         }); 
     };  
 
@@ -32,6 +33,19 @@ const Comments  = ({currentUserId}) => {     // we need to pass in our props the
             });
         }
     };
+
+    const updateComment = (text, commentId) => {
+        updateCommentApi(text, commentId).then(() => { 
+            const updatedBackendComments = backendComments.map(backendComment => {
+                if (backendComment.id === commentId) {
+                    return {...backendComment, body: text}
+                }
+                return backendComment
+            });
+            setbackendComments(updatedBackendComments);
+            setActiveComment(null);
+        })
+    }
 
     useEffect(() => {
         getCommentsApi().then(data => {      //the data will be the array of the backendComments
@@ -55,6 +69,10 @@ const Comments  = ({currentUserId}) => {     // we need to pass in our props the
         replies={getReplies(rootComment.id)}
         currentUserId={currentUserId}
         deleteComment={deleteComment}
+        updateComment={updateComment}
+        activeComment={activeComment}
+        setActiveComment={setActiveComment}
+        addComment={addComment}
         />
        ))}      
        </div>   
